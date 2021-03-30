2ebe558282e5eeff356d328f6a1bf8c0ef78cc9a
jzenn <johannes.zenn@icloud.com>
2021-03-30 14:54:56 +0200

    deleted linalg subpackage

--- cluster/linalg/__init__.py
+++
@@ -1,6 +0,0 @@
-from .power import reighleigh_quotient, power, get_first_k_eig, get_last_k_eig
-from .functions import get_matrix_str
-from .similarity import get_similarity_matrix
-
-__all__ = ['reighleigh_quotient', 'power', 'get_first_k_eig', 'get_last_k_eig', 'get_matrix_str',
-           'get_similarity_matrix']
