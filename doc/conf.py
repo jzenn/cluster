e90b876ac19edd839c9a5d2d584efb3e2a47f316
jzenn <johannes.zenn@icloud.com>
2021-04-01 10:56:19 +0200

    0.0.2 -> 0.1.0

--- doc/conf.py
+++ doc/conf.py
@@ -27,7 +27,7 @@ copyright = "2021, Johannes Zenn"
 author = "Johannes Zenn"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.0"
+release = "0.1.0"
 
 
 # -- General configuration ---------------------------------------------------
