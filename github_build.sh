7285b89f36ca434a47ff6aae21ca4d02dcda5615
jzenn <johannes.zenn@icloud.com>
2021-04-01 08:50:52 +0200

    updated github build script

--- github_build.sh
+++ github_build.sh
@@ -12,14 +12,17 @@ mv doc/_build ~/Desktop/
 
 # checkout gh-pages and replace content
 git checkout -B gh-pages
+setopt rmstarsilent
 rm -rf *
-mv ~/Desktop/_site/* .
-rmdir ~/Desktop/_site
+mv ~/Desktop/_build/* .
+mv ~/Desktop/_build/.* .
+rmdir ~/Desktop/_build
 
 # commit to gh-build
+touch .nojekyll
 git add .
 git commit -m "site updated at $(date)"
-git push origin gh-build --force
+git push origin gh-pages --force
 git checkout master
 
 # remove all untracked files
