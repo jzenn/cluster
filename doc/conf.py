023acc395179833356b8678bfc872658b998bd82
jzenn <johannes.zenn@icloud.com>
2021-04-01 10:43:29 +0200

    updated documentation configuration

--- doc/conf.py
+++ doc/conf.py
@@ -58,6 +58,12 @@ html_theme = "alabaster"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
+
 ## Add __init__ parameters and descriptions
 autoclass_content = "both"
 
